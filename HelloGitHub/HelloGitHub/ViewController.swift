8c35f34c16cf95a175b5c6ce0d04f170b33c2161
mahabaleshwar hegde <mahabaleshwarhegde@mahabaleshwars-MacBook-Pro.local>
2019-03-24 11:01:43 +0530

    - added view will appear

--- HelloGitHub/HelloGitHub/ViewController.swift
+++ HelloGitHub/HelloGitHub/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("hello")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 
 }
 
